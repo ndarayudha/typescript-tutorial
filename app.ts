fac7db69377490155a63907e294b5a832c4d5513
yofan2408 <yofan.ixe@gmail.com>
2021-12-01 01:01:53 +0700

    add enum

--- app.ts
+++ app.ts
@@ -9,28 +9,45 @@
 
 // console.log(person);
 
-const person: {
-    name: string;
-    age: number;
-    hobbies: string[];
-    role: [number, string]
-} = {
-  name: "Yofan",
-  age: 21,
-  hobbies: ['Gaming', 'Learning'],
-  role: [2, 'author']
-};
+// const person: {
+//     name: string;
+//     age: number;
+//     hobbies: string[];
+//     role: [number, string]
+// } = {
+//   name: "Yofan",
+//   age: 21,
+//   hobbies: ['Gaming', 'Learning'],
+//   role: [2, 'author']
+// };
 
 // person.role.push('admin'); // ERROR tuple
 // person.role[1] = 10;        // ERROR tuple
 // person.role = [12, "sdlfj"] // correct using tuple
 
+// ENUMS
+enum Role {
+  ADMIN,
+  READ_ONLY,
+  AUTHOR,
+}
+
+const person = {
+  name: "Yofan",
+  age: 21,
+  hobbies: ["Gaming", "Learning"],
+  role: Role.ADMIN,
+};
+
+if (person.role === Role.ADMIN) {
+  console.log("this is admin");
+}
+
 let favoriteActivities: string[];
-favoriteActivities = ['Gaming', 'Learning']
+favoriteActivities = ["Gaming", "Learning"];
 
 console.log(person);
 
-
-for (const hobby of person.hobbies){
-    console.log(hobby);
+for (const hobby of person.hobbies) {
+  console.log(hobby);
 }
