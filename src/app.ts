9b231ccc22d5088b7908063217f76b0260f3679a
yofan2408 <yofan.ixe@gmail.com>
2021-12-03 16:02:25 +0700

    finish course

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+// config js target
+// let age: number;
+// age = 22;
+// const userName = "Yofan";
+// console.log('Yofan');
+
+
+const button = document.querySelector('button');
+
+button?.addEventListener('click', () => {
+    console.log('Clicked!');
+});
